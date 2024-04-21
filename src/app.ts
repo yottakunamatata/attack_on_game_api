ab08b8de4a20ae15ec1200f14700d5caaed5fa75
Kami <106948222+KamiiLiu@users.noreply.github.com>
2024-04-21 15:28:13 +0800

    feat:修正readme的內容 (#1)

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,5 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 console.log('你安装成功哩QQ');
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
 export default app;
 
