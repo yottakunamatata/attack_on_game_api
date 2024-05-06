d1ce02e6c850fd125b11ec03f09676cee8ed8a9c
Kami <106948222+KamiiLiu@users.noreply.github.com>
2024-05-06 15:26:33 +0800

    feat:建立prettier,eslint,render環境 (#4)
    * feat:建立render環境
    * style:建立prettier和eslint環境

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
-console.log('你安装成功哩QQ');
+app.use(cors());
 export default app;
-
--- src/service/server.ts
+++ src/service/server.ts
@@ -1,8 +1,9 @@
 import app from '../app';
+import { Request, Response } from 'express';
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`你現在收看的是http://localhost:${PORT}`);
 });
-app.get('/', (req, res) => {
-    res.send('莎莎給油');
-  });
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: '莎莎給油!' });
+});
