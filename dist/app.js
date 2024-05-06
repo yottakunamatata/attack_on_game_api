f635c774fc4081ef6c5d4a8b76b22c17678e8802
Kami <106948222+KamiiLiu@users.noreply.github.com>
2024-05-06 15:20:47 +0800

    style:建立prettier和eslint環境 (#3)
    * feat:建立prettier和eslint環境，確保存欓格式都漂漂亮亮

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,13 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
+const cors_1 = __importDefault(require('cors'));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-console.log('你安装成功哩QQ');
-// app.get('/', (req, res) => {
-//     res.send('Hello, World!');
-//   });
+app.use((0, cors_1.default)());
 exports.default = app;
--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 # AttackOnGame API
+
 ## 項目概述
 
 AttackOnGame API是桌遊揪團網站【聚人】使用的後端API項目，目前是由Express框架構建，並通過Mongoose與MongoDB數據庫交互，支持跨域請求處理。
 
 ## 如何安装
+
 ```bash
 git clone https://your-repository-url/attackongame_api.git
 cd attackongame_api
@@ -11,14 +13,35 @@ npm install
 ```
 
 ## 如何運行
-### Development
+
+### 開發期指令
+
 ```bash
 npm run dev
-http://localhost:3000/
-```
-###
-```bash
-npm start 
-# 不是很確定是不是這樣寫QQ
 ```
 
+開啟開發模式，伺服器運行在 http://localhost:3000/
+
+### 編譯 TypeScript
+
+```bash
+npm run build
+```
+
+將 TypeScript 代碼編譯成 JavaScript
+
+### 直接執行 TypeScript 代碼
+
+```bash
+npm start
+```
+
+直接運行 TypeScript 代碼
+
+### 格式化 TypeScript 代碼
+
+```bash
+npm run format
+```
+
+使用 Prettier 工具格式化所有 TypeScript 文件。
