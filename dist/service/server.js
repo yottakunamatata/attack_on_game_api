e4beb0a22d896aa8a51cd11dfaf5421e220bf991
ML-Kami <125852208+ML-Kami@users.noreply.github.com>
2024-05-18 16:58:08 +0800

    feat(vscode): 添加特定的配置設定 (#7)
    新增了對於 ESLint 的自動修正設定
    優化了 debugger 配置以支持新的 Node.js 版本
    更新了工作區的設定以包括更多的文件排除規則

--- dist/service/server.js
+++ dist/service/server.js
@@ -1,15 +1,13 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const app_1 = __importDefault(require('../app'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("../app"));
 const PORT = process.env.PORT || 3000;
 app_1.default.listen(PORT, () => {
-  console.log(`你現在收看的是http://localhost:${PORT}`);
+    console.log(`你現在收看的是http://localhost:${PORT}`);
 });
 app_1.default.get('/', (req, res) => {
-  res.status(200).json({ message: '莎莎給油!' });
+    res.status(200).json({ message: '莎莎給油!' });
 });
