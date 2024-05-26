facf86607497a09f1ad40a4d4c26488fadcbef8a
Benson <eagle163013@gmail.com>
2024-05-26 19:53:29 +0800

    chore: Update user router to use user validator for create and update operations

---
+++ src/validator/user.ts
@@ -0,0 +1,31 @@
+
+import { body } from 'express-validator';
+import { UserRole } from '../models/User';
+
+export const userCreateValidator = [
+    body('email')
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email is invalid'),
+    body('password')
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 8 }).withMessage('Password is too short'),
+    body('role')
+        .notEmpty().withMessage('Role is required')
+        .isIn(Object.values(UserRole)).withMessage('Role is invalid'),
+]
+
+export const userUpdateValidator = [
+    body('password')
+        .optional()
+        .isLength({ min: 8 }).withMessage('Password is too short'),
+]
+
+export const loginValidator = [
+    body('email')
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email is invalid'),
+    body('password')
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 8 }).withMessage('Password is too short'),
+]
+
