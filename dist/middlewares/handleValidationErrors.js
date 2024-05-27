ff6e2b769e2a86f6a7d9a841d9da80b4e34a4278
ML-Kami <125852208+ML-Kami@users.noreply.github.com>
2024-05-27 18:06:28 +0800

    feature/add-express-validator-and-module-alias (#10)
    feat: 新增 express-validator
    feat: 新增 module-alias/register

---
+++ dist/middlewares/handleValidationErrors.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handleValidationErrors = void 0;
+const express_validator_1 = require("express-validator");
+const handleValidationErrors = (req, res, next) => {
+    const errors = (0, express_validator_1.validationResult)(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ message: errors.array().map((x) => x.msg) });
+        return;
+    }
+    next();
+};
+exports.handleValidationErrors = handleValidationErrors;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import generateJWT from '../middlewares/generateJWT';
 import { jwtAuthenticator, localAuthenticator } from '../middlewares/auth';
-import UserRouter from './user';
+import UserRouter from '@/routers/user';
+import EventRouter from '@/routers/event';
 const router = Router();
 
 router.use('/user', UserRouter);
 router.post('/login', localAuthenticator, generateJWT);
-
+router.use('/event', EventRouter);
 router.get('/profile', jwtAuthenticator, (req, res) => {
   res.status(200).json({ user: req.user });
 });
