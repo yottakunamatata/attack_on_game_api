e767afdbaf990a2a900f2dc71752d385d3b4cae0
Berutorion <49361351+Berutorion@users.noreply.github.com>
2024-06-09 13:30:32 +0800

    Feat-forget-password (#30)
    * chore: Add password router and controller, and update index router
    
    * feat: Add email validation code functionality for password reset
    
    * chore: Add reset password functionality
    
    * chore: Add change password functionality
    
    * chore: Hash user password before saving
    
    * chore: Update .env.example and password controller to include email and Google OAuth configurations

---
+++ dist/controllers/password.js
@@ -0,0 +1,88 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.changePassword = exports.resetPassword = exports.sendResetPasswordEmail = void 0;
+const User_1 = __importDefault(require("../models/User"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const bcrypt_1 = require("bcrypt");
+const help_1 = require("@/utils/help");
+const help_2 = require("@/utils/help");
+const sendResetPasswordEmail = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { to, fronEndUrl } = req.body;
+        const user = yield User_1.default.findOne({ email: to });
+        if (!user) {
+            res.status(404).json({ status: false, message: "User not found" });
+            return;
+        }
+        const validateCode = Math.floor(100000 + Math.random() * 900000).toString();
+        const validationToken = jsonwebtoken_1.default.sign({ email: user.email, emailCode: validateCode }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        user.emailCode = validateCode;
+        yield user.save();
+        yield (0, help_2.sendEamilValidationCode)(to, validationToken, fronEndUrl);
+        res.status(200).json({ status: true, message: "Email sent" });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ status: false, message: err.message });
+    }
+});
+exports.sendResetPasswordEmail = sendResetPasswordEmail;
+const resetPassword = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { validationToken, newPassword } = req.body;
+        const decoded = jsonwebtoken_1.default.verify(validationToken, process.env.JWT_SECRET);
+        const user = yield User_1.default.findOne({ email: decoded.email });
+        if (!user) {
+            res.status(404).json({ status: false, message: "User not found" });
+            return;
+        }
+        if (user.emailCode !== decoded.emailCode) {
+            res.status(400).json({ status: false, message: "Invalid code" });
+            return;
+        }
+        user.password = newPassword;
+        user.emailCode = "";
+        yield user.save();
+        res.status(200).json({ status: true, message: "Password has be reseted" });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ status: false, message: err.message });
+    }
+});
+exports.resetPassword = resetPassword;
+const changePassword = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { oldPassword, newPassword } = req.body;
+        const user = yield User_1.default.findById((0, help_1.getUser)(req)._id);
+        if (!user) {
+            res.status(404).json({ status: false, message: "User not found" });
+            return;
+        }
+        const isPasswordValid = (0, bcrypt_1.compare)(oldPassword, user.password);
+        if (!isPasswordValid) {
+            res.status(400).json({ status: false, message: "Invalid password" });
+            return;
+        }
+        user.password = newPassword;
+        yield user.save();
+        res.status(200).json({ status: true, message: "Password changed" });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ status: false, message: err.message });
+    }
+});
+exports.changePassword = changePassword;
--- src/utils/help.ts
+++ src/utils/help.ts
@@ -1,6 +1,8 @@
 import { Request } from 'express';
 import { IUser } from '../models/User';
 import { RequestWithUser } from '../types/commonRequest';
+import { google } from 'googleapis';
+import nodemailer from 'nodemailer';
 
 function isRequestWithUser(req: any): req is RequestWithUser {
   return req.user !== undefined;
@@ -13,3 +15,41 @@ export const getUser = (req: Request) => {
   const user = req.user as IUser;
   return user;
 };
+
+export const sendEamilValidationCode = async (to: string, validationToken: string, frontEndUrl: string) => {
+  const OAuth2 = google.auth.OAuth2;
+
+  const oauth2Client = new OAuth2({
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    redirectUri: "https://developers.google.com/oauthplayground"
+  });
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+  });
+
+  const { token } = await oauth2Client.getAccessToken();
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL_ADDRESS,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      accessToken: token
+    }
+  })
+
+  const mailOptions = {
+    from: process.env.EMAIL_ADDRESS as string,
+    to,
+    subject: "Reset your password",
+    text: `Click the link to reset your password: ${frontEndUrl}/${validationToken}`
+  };
+
+  await transporter.sendMail(mailOptions);
+
+}
