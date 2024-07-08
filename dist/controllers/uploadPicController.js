272e3c01646c942d7b227f168f9458a65a6aac97
vocalise.com <vocalise.com@gmail.com>
2024-07-08 21:46:53 +0800

    圖片上傳：extension檔名修正

--- dist/controllers/uploadPicController.js
+++ dist/controllers/uploadPicController.js
@@ -100,8 +100,7 @@ const uploadPic = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             return res.status(500).send({ message: 'Route輸入格式錯誤' });
         }
     }
-    catch (error) {
-    }
+    catch (error) { }
 });
 exports.uploadPic = uploadPic;
 // 取得檔案夾內圖片
@@ -122,7 +121,6 @@ const getPics = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
                     action: 'read',
                     expires: '12-31-2500',
                 });
-                console.log(file.name);
                 fileList.push({
                     fileName: file.name,
                     imgUrl: fileUrl,
